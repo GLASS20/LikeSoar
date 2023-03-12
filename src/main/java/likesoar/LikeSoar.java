3f666b676b28f20a30f6a22a91234dbace5c1ce0
HyuiChan <lyglass20@gmail.com>
2023-03-12 22:50:51 +0800

    Initial commit

---
+++ src/main/java/likesoar/LikeSoar.java
@@ -0,0 +1,14 @@
+package likesoar;
+
+public class LikeSoar {
+    LikeSoar likeSoar = new LikeSoar();
+    public static final String name = "LikeSoar";
+
+    public void OnStart() {
+        // todo: on client starting
+    }
+
+    public void OnExit() {
+        // todo: on client exiting
+    }
+}
